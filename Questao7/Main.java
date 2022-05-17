cdbe4d1284841b8aeab5d55c1b7e10bbacbdc5c7
Falik <123Mudar!>
2022-05-17 09:08:27 -0300

    Comentarios questao 7

--- Questao7/Main.java
+++ Questao7/Main.java
@@ -10,12 +10,15 @@ public class Main {
     public static void main(String[] args) {
         String string = "Exemplo de uso de métodos para Strings";
         for (int i = 0, len = string.length(); i < len; i++) {
+            //for para percorrer a string inteira em char
             char ch = string.charAt(i);
             String str = ch + "";
             if (i % 2 == 0) {
                 System.out.print(str.toUpperCase());
+                //se for par vai pra upper case
             } else {
                 System.out.print(str.toLowerCase());
+                //se for ímpar vai pra lower case
             }
         }
     }
