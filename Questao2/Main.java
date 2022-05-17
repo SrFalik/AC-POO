19bbe2f81449dbd07375d8fc166551a80ea11e72
Falik <123Mudar!>
2022-05-17 08:56:14 -0300

    Comentarios questao 2

--- Questao2/Main.java
+++ Questao2/Main.java
@@ -8,21 +8,19 @@ package Questao2;
 
 public class Main {
     public static void main(String[] args) {
-        int i;
-        int numero;
-        StringBuilder primos = new StringBuilder();
+        int i, numero;
 
         for (i = 1; i <= 100; i++) {
             int contador = 0;
             for (numero = i; numero >= 1; numero--) {
                 if (i % numero == 0) {
-                    contador = contador + 1;
+                    contador++;
                 }
+                //Verificando se é par, ou seja não é primo
             }
             if (contador == 2) {
-                primos.append(i).append(" ");
+                System.out.print(i + " ");
             }
         }
-        System.out.println(primos);
     }
 }
