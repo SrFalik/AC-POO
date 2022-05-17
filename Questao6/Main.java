23362e4041983a39c73921cc89e95d8686420b67
Falik <123Mudar!>
2022-05-17 09:07:02 -0300

    Comentarios questao 6

--- Questao6/Main.java
+++ Questao6/Main.java
@@ -12,6 +12,7 @@ public class Main {
         String texto = "Os nomes das classes em java precisam começar com uma letra e depois destas quaisquer combinações de letras e dígitos. Como padrão, os nomes são substantivos que iniciam com uma letra maiúscula. Para as constantes é costume utilizar sempre letras maiúsculas nos nomes.";
 
         long qtd = texto.chars().filter(l -> l == 'a' || l == 'A' || l == 'ã' || l == 'á' || l == 'â').count();
+        //separando o texto em char e filtrando todos os "a's" possíveis, depois adicionando no long quantidade
 
         System.out.println(qtd);
     }
