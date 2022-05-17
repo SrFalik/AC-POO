236a179211c350e549513efe8ca7d650bc78c69f
Falik <123Mudar!>
2022-05-17 08:50:50 -0300

    Comentarios questao 1

--- Questao1/Main.java
+++ Questao1/Main.java
@@ -9,14 +9,15 @@ package Questao1;
 class Main {
     public static void main(String[] args) {
         int numero1 = 0, numero2 = 1;
-        int contador = 0;
-        while (contador < 15) {
+            for (int i = 0; i < 15; i++) {
+            //Fibonacci = Os dois números anteriores somados é igual ao próximo número
             System.out.print(numero1 + " ");
             int numero3 = numero2 + numero1;
+            //Setando o próximo número
             numero1 = numero2;
             numero2 = numero3;
-            contador = contador + 1;
-        }
+            //Igualando os números para fazer próximo número da sequência
+            }
 
     }
 }
