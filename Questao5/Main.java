a4faae39e40f23612bb756f7e097a0cac46d6a66
Falik <123Mudar!>
2022-05-17 09:05:51 -0300

    Comentarios questao 5

--- Questao5/Main.java
+++ Questao5/Main.java
@@ -11,17 +11,17 @@ public class Main {
         long n = 1, p;
         int qtd = 0;
         while (qtd < 4) {
+            //while para verificar se o número é perfeito, até 4 números
             p = perfeito(n);
             if (p == n) {
                 System.out.println(n);
                 qtd++;
-                n++;
-            } else {
-                n++;
             }
+                n++;
         }
     }
     static long perfeito(long numero) {
+        //Método para verificar número perfeito
         long soma = 0;
         for (int i = 1; i <= numero / 2; i++) {
             if (numero % i == 0) {
